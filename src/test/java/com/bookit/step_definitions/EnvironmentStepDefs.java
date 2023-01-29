58bb3ddac44415bd4c60076fc2cbf4d550cde121
SpeedyInsect777 <manasoev@gmail.com>
2023-01-28 23:56:21 -0500

    last update

--- src/test/java/com/bookit/step_definitions/EnvironmentStepDefs.java
+++ src/test/java/com/bookit/step_definitions/EnvironmentStepDefs.java
@@ -7,15 +7,12 @@ import org.junit.Test;
 
 public class EnvironmentStepDefs {
 
-
     @Given("I get related environment information")
     public void i_get_related_environment_information() {
-
 
         System.out.println("Environment.URL = " + Environment.URL);
         System.out.println("Environment.BASE_URL = " + Environment.BASE_URL);
         System.out.println("Environment.DB_URL = " + Environment.DB_URL);
-
 
     }
 
@@ -37,10 +34,4 @@ public class EnvironmentStepDefs {
         System.out.println("browserEx = " + browserEx);
 
     }
-
-
-
-
-
-
 }
